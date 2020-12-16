84b4ae1008c16d25db4cc23160f170bfb64ddbf3
just1ce415 <53081256+just1ce415@users.noreply.github.com>
2020-12-16 11:06:47 +0200

    skeleton

---
+++ relations_tools.py
@@ -0,0 +1,56 @@
+"""docstring"""
+
+def read_relation(path):
+    '''
+    (str) -> list(list)
+    '''
+    pass
+
+
+def write_relation(path, relation):
+    '''
+    (str, list(list)) ->
+    '''
+    pass
+
+
+def get_reflexive_closure(relation):
+    '''
+    (list(list)) -> list(list)
+    '''
+    pass
+
+
+def get_symmetric_closure(relation):
+    '''
+    (list(list)) -> list(list)
+    '''
+    pass
+
+
+def get_transitive_closure(relation):
+    '''
+    (list(list)) ->l ist(list)
+    '''
+    pass
+
+
+def is_transitive(relation):
+    '''
+    (list(list)) -> bool
+    '''
+    pass
+
+
+def get_equivalence_classes(relation):
+    '''
+    (list(list)) -> list(list)
+    '''
+    pass
+
+
+def get_number_transitive_relations(n):
+    '''
+    (int) -> int
+    '''
+    pass
