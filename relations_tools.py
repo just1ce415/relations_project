0f31faa5858856f4e1e579639c7b62635b5cfbe2
Kolotose <76049300+Kolotose@users.noreply.github.com>
2021-01-04 19:06:19 +0200

    Update relations_tools.py
    A prefinal version of a beta of relation_tools.py

--- relations_tools.py
+++ relations_tools.py
@@ -1,80 +1,122 @@
 """
-The module with functions to operate and process with relations based on the martix representation.
-Project repository: https://github.com/just1ce415/relations_project
+This module is created to operate and process with
+relations based on the martix representation
+(every element[row][column] is presetented as:
+1 - if element IS in relation
+0 - if element IS NOT in relation)
+
+Project repository:
+https://github.com/just1ce415/relations_project
 """
 import copy
 
 def read_relation(path: str) -> list:
     '''
     (str) -> list(list)
-    Reads a relation (matrix) from the file stated as (path) and returns the list of lists
-    Supported separator: ',' (csv file).
-    If the file has not .csv format, the function will return None.
-    '''
-    with open(path, 'r', encoding='utf=8') as rel_f:
-        relation_lst = []
-        for line in rel_f:
-            if line.find(', ') != -1:
-                sep = ', '
-            elif line.find(' ') != -1:
-                sep = ' '
-            else:
-                sep = ','
 
+    Reads a relation (matrix) from the file stated as [path]
+    and returns the list of lists
+
+    Supported separators: ","   ", "   " "(csv file).
+    '''
+    with open(path, 'r', encoding='utf=8') as relation_file:
+        relation_list = []
+
+        # Searching for separator in line
+        for line in relation_file:
+            if line.find(', ') != -1:
+                separator = ', '
+            elif line.find(' ') != -1:
+                separator = ' '
+            else:
+                separator = ','
+
+            # The last line of the file
             if line == '':
                 break
-            # We always have csv file.
-            row = line[:-1].split(sep)
-            for i in range(len(row)):
-                row[i] = int(row[i])
-            relation_lst.append(row)
-        return relation_lst
+
+            # Cutting off '\n' and turning into list (rows of matrix)
+            row = line[:-1].split(separator)
+
+            # Turning every element of matrix into integer number
+            # and adding the row to the matrix
+            for index in range(len(row)):
+                row[index] = int(row[index])
+            relation_list.append(row)
+
+        return relation_list
 
 
 def write_relation(relation: list, path: str='relation_out.csv'):
     '''
-    Writes relation (matrix) to the file with (path). The function create relation_out.csv in
+    list(list) -> file
+
+    Writes relation (matrix) to the file with (path).
+    The function creates relation_out.csv in
     the project root directory by default.
     '''
-    with open(path, 'w', encoding='utf-8') as rel_f:
+    with open(path, 'w', encoding='utf-8') as relation_file:
+
+        # Writing every row into file with relation
         for row in relation:
-            row = [str(el) for el in row]
-            rel_f.write(','.join(row) + '\n')
+            # Turning every element into string and joining it into file
+            row = [str(element) for element in row]
+            relation_file.write(','.join(row) + '\n')
 
 
-def get_symmetric_closure(lst: list) -> list:
+def get_symmetric_closure(relation: list) -> list:
     """
-    Receives the list of lists with matrix and returns
-    the list with this matrix's reflexive closure.
+    list(list) -> list(list)
+
+    Receives the list of lists with matrix and
+    returns the list with this matrix's symmetric closure.
+
     >>> get_symmetric_closure([[0, 1, 0, 0, 0], [1, 1, 1, 0, 1], \
 [1, 1, 1, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 0, 0]])
     [[0, 1, 1, 0, 0], [1, 1, 1, 0, 1], [1, 1, 1, 0, 0], \
 [0, 0, 0, 1, 1], [0, 1, 0, 1, 0]]
+    >>> get_symmetric_closure([[0, 1, 0, 0], [1, 1, 1, 1], \
+[0, 0, 1, 0], [0, 0, 1, 0]])
+    [[0, 1, 0, 0], [1, 1, 1, 1], [0, 1, 1, 1], [0, 1, 1, 0]]
     """
-    for row in range(len(lst)):
-        for column in range(len(lst)):
-            if lst[row][column] == 1:
-                lst[column][row] = 1
-    return lst
+    # Going through every element of matrix
+    for row in range(len(relation)):
+        for column in range(len(relation)):
+
+            # If element (a, b) in relation, add (b, a)
+            if relation[row][column] == 1:
+                relation[column][row] = 1
+
+    return relation
 
 
-def get_reflexive_closure(lst: list) -> list:
+def get_reflexive_closure(relation: list) -> list:
     """
-    Receives the list of lists with bool matrix and
-    returns its symetric closure.
-    >>> get_reflexive_closure([[0, 0, 0], [0, 1, 0], \
-[0, 0, 0]])
+    list(list) -> list(list)
+
+    Receives the list of lists with matrix and
+    returns the list with this matrix's reflexive closure.
+
+    >>> get_reflexive_closure([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
     [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    >>> get_reflexive_closure([[0, 1, 0, 0], [1, 1, 1, 1], \
+[0, 0, 1, 0], [0, 0, 1, 0]])
+    [[1, 1, 0, 0], [1, 1, 1, 1], [0, 0, 1, 0], [0, 0, 1, 1]]
     """
-    for index in range(len(lst)):
-        lst[index][index] = 1
-    return lst
+    for index in range(len(relation)):
+        # Add element, that may be represented as (a, a)
+        relation[index][index] = 1
+
+    return relation
 
 
 def get_transitive_closure(relation: list) -> list:
     '''
-    Takes a relation (matrix) and using Warshall's Algorithm return it's
-    transitive closure as list of lists.
+    list(list) -> list(list)
+
+    Receives the list of lists with matrix and using Warshall's Algorithm
+    transforms and returns the list with this matrix's transitive closure.
+
     >>> get_transitive_closure([[0 ,0 ,0 ,1], [1, 0, 1, 0], \
 [1 ,0 ,0 ,1], [0, 0, 1, 0]])
     [[1, 0, 1, 1], [1, 0, 1, 1], [1, 0, 1, 1], [1, 0, 1, 1]]
@@ -93,92 +135,122 @@ def get_transitive_closure(relation: list) -> list:
             for j in range(len(relation)):
                 # W(k)[i][j] = W(k-1)[i][j] or (W(k-1)[i][k] and W(k-1)[k][j])
                 relation[i][j] = relation[i][j] | (relation[i][k] & relation[k][j])
+
     return relation
 
 
-def is_transitive(matrix: list) -> bool:
+def is_transitive(relation: list) -> bool:
     '''
-    Check if matrix (relation) is transitive.
+    list(list) -> bool
+
+    Check if matrix with relation (list of lists) is transitive
+    using Warshall's Algorithm.
+    (If transitive closure of relation is equal to relation
+    then it is itransitive)
+
     >>> is_transitive([[1, 0, 1], [1, 1, 0], [0, 0, 1]])
     False
     >>> is_transitive([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], \
 [0, 1, 1, 0, 0], [0, 1, 1, 1, 0]])
     True
+    >>> is_transitive([[1, 0, 1, 1], [1, 0, 1, 1], [1, 0, 1, 1], [1, 0, 1, 1]])
+    True
     '''
-    if len(matrix) == 0:
+    # If relation has no elements it is already transitive
+    if len(relation) == 0:
         return True
-    trans_closure = copy.deepcopy(matrix)
-    for k in range(len(matrix)):
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                trans_closure[i][j] = trans_closure[i][j] | (trans_closure[i][k] & trans_closure[k][j])
-                if matrix[i][j] != trans_closure[i][j]:
+
+    # t_closure -- transitive closure of the relation
+    t_closure = copy.deepcopy(relation)
+    # k index states for W(k) matrix
+    for k in range(len(relation)):
+        for i in range(len(relation)):
+            for j in range(len(relation)):
+                # W(k)[i][j] = W(k-1)[i][j] or (W(k-1)[i][k] and W(k-1)[k][j])
+                t_closure[i][j] = t_closure[i][j] | (t_closure[i][k] & t_closure[k][j])
+
+                # If only one element has a diference, it is already not transitive
+                if relation[i][j] != t_closure[i][j]:
                     return False
+
     return True
 
 
-def get_equivalence_class(lst: list) -> list:
+def get_equivalence_classes(relation: list) -> list:
     """
-    Receives a matrix in list of lists and returns all
-    its equivalence classes in list of lists.
-    >>> get_equivalence_class([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], \
+    list(list) -> list(list)
+
+    Receives the list of lists with matrix of relation of equivalence and
+    returns all its equivalence classes as list of lists.
+
+    >>> get_equivalence_classes([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], \
 [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
     [[0], [1], [2], [3], [4]]
+    >>> get_equivalence_classes([[1, 0, 1, 0], [0, 1, 0, 0], \
+[1, 0, 1, 0], [0, 0, 0, 1]])
+    [[0, 2], [1], [3]]
     """
-    equivalence_dict = {key: set() for key in range(len(lst))}
-    length = len(lst)
+    # Creating a blank for equivalence classes
+    equivalence_dictionary = {key: set() for key in range(len(relation))}
+    length = len(relation)
 
-    [equivalence_dict[row].add(column)
+    # Adding an element to a needed equivalence class
+    [equivalence_dictionary[row].add(column)
         for row in range(length)
-        for column in range(length) if lst[row][column] == 1]
+        for column in range(length) if relation[row][column] == 1]
 
+    # Creating final classes of equivalence
     result = []
-    [result.append(list(element)) for element in equivalence_dict.values()
+    [result.append(list(element)) for element in equivalence_dictionary.values()
     if list(element) not in result]
+
     return result
 
 
-def is_transitive_1(relation):
+def is_transitive_alternative(relation):
     '''
     (list(list)) -> bool
 
-    This function is used for function get_number_of_transitive_relations
+    This function is used for function get_number_of_transitive
 
-    Check if relation is transitive.
+    Checks if [relation] is transitive.
 
-    >>> is_transitive_1([(1, 2), (2, 3), (1, 3)])
+    >>> is_transitive_alternative([(1, 2), (2, 3), (1, 3)])
     True
-    >>> is_transitive_1([(1, 1), (2, 2), (1, 3)])
+    >>> is_transitive_alternative([(1, 1), (2, 2), (1, 3)])
     False
     '''
     second_elements = [b for (a, b) in relation]
     for (a, b) in relation:
         for c in second_elements:
+            # if (a, b) in relation, (b, c) in relation
+            # and (a, c) not in relation -> False 
             if (b, c) in relation and (a,c) not in relation:
                 return False
+
     return True
 
-def get_number_of_transitive_relations(number_of_elements):
+def get_number_of_transitive(number_of_elements):
     """
     int -> int
 
     WARNING!!!
-    DO NOT USE THIS VERSION OF FUNCTION FOR NUMBERS HIGHER THAN 5
+    DO NOT USE THIS FUNCTION FOR NUMBERS HIGHER THAN 5
     IT MAY LEAD TO CALCULATIONS THAT WILL CONTINUE FOR MORE THAN
-    2000 MINUTES
+    40 HOURS
 
     Counts a number of transitive relations on set {A x A}
     where A has [number_of_elements] elements
 
-    >>> get_number_of_transitive_relations(0)
+    >>> get_number_of_transitive(0)
     1
-    >>> get_number_of_transitive_relations(1)
+    >>> get_number_of_transitive(1)
     2
-    >>> get_number_of_transitive_relations(2)
+    >>> get_number_of_transitive(2)
     13
-    >>> get_number_of_transitive_relations(3)
+    >>> get_number_of_transitive(3)
     171
-    >>> get_number_of_transitive_relations(4)
+    >>> get_number_of_transitive(4)
     3994
     """
     A = [x for x in range(number_of_elements)]
@@ -203,13 +275,13 @@ def get_number_of_transitive_relations(number_of_elements):
         for index in range(len(binary_set)):
             if binary_set[index] == 1:
                 relation.append(main_relation[index])
-        
+
         # Prepares the blank for the next relation
         binary_set[0] += 1
 
         # Checks if the relation is transitive
         # If yes, then includes it in the amount of transitive relations
-        if is_transitive_1(relation):
+        if is_transitive_alternative(relation):
             amount_of_transitive_relations += 1
 
     return amount_of_transitive_relations + 1
@@ -217,29 +289,30 @@ def get_number_of_transitive_relations(number_of_elements):
 
 
 if __name__ =='__main__':
-    # Read
-    relation = read_relation('rel_1500_0.9.csv')
-    print('ended')
+    #~~~Read~~~
+    #relation = read_relation('rel_1500_0.45.csv')
+    #print('read ended')
 
-    ## Closures
+    #~~~Closures~~~
     #reflexive = get_reflexive_closure(relation)
     #print('refl ended')
     #symmetric = get_symmetric_closure(relation)
     #print('symm ended')
-    transitive = get_transitive_closure(relation)
-    print('trans ended')
+    #transitive = get_transitive_closure(relation)
+    #print('trans ended')
 
-    ## Equivalance classes
-    #eqiuv_classes = get_equivalence_class(relation)
+    #~~~Equivalance classes~~~
+    #eqiuv_classes = get_equivalence_classes(relation)
     #print('eq_clss ended')
 
-    # Is_transitive
+    #~~~Is_transitive~~~
     #trans = is_transitive(relation)
     #print('is_trans')
 
-    #num = get_number_of_transitive_relations(5)
-    #print('ended')
+    #~~~Number_of_transitive~~~
+    #num = get_number_of_transitive(5)
+    #print('num_of_tr ended')
 
-    # Write
+    #~~~Write~~~
     #write_relation(relation)
-    #print('ended')
+    #print('write ended')
